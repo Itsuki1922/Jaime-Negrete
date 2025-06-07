b7c6128623dc4746211d3371447ea590414196bb
Jaime1922 <JaimeNF.1922@gmail.com>
2025-06-07 20:26:52 +0200

    1Dia

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import styles from './HeroStyles.module.css';
+
+
+function Hero(){
+    return ( 
+    <section id="hero">
+        <div></div>
+        <div></div>
+    </section>
+    );
+}
+
+export default Hero;
