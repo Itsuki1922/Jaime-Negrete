4e9b9c0d4cd377ce3a9e841dfbfbf58405672c0e
Jaime1922 <JaimeNF.1922@gmail.com>
2025-06-08 16:45:13 +0200

    1.01

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,11 +1,33 @@
 import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.png';
+import themeIcon from '../../assets/sun.svg';
+import LinkedinIcon from '../../assets/linkedin-light.svg';
+import GithubIcon from '../../assets/github-light.svg';
+import CV from '../../assets/cv.pdf';
 
 
 function Hero(){
     return ( 
-    <section id="hero">
-        <div></div>
-        <div></div>
+    <section id="hero" className={styles.container}>
+        <div className={styles.colorModeContainer}>
+            <img className={styles.hero} src={heroImg} alt="Imagen Perfil"/>
+            <img className={styles.colorMode} src={themeIcon} alt="color mode"/>
+        </div>
+        <div className={styles.info}>
+            <h1>Jaime <br /> Negrete</h1>
+            <h2>Frontend Developer</h2>
+            <span>
+                <a href="https://linkedin.com/in/jaimenegreteferrerdesarrollador" target='_blank'>
+                    <img src={LinkedinIcon} alt="Icono Linkedin" />
+                </a>
+                <a href="https://github.com" target='_blank'>
+                    <img src={GithubIcon} alt="Icono Github" />
+                </a>
+            </span>
+            <a href={CV} download>
+                <button type="button">Curriculum Vitae</button>
+            </a>
+        </div>
     </section>
     );
 }
